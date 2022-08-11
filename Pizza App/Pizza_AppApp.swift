1fc0bfb34769817da9b024eb6cfae1c4fcfaf76e
Athif Shaffy <athif.shaffy@gmail.com>
2022-08-12 00:54:13 +0530

    Initial Commit

---
+++ Pizza App/Pizza_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pizza_AppApp.swift
+//  Pizza App
+//
+//  Created by test on 2022-08-12.
+//
+
+import SwiftUI
+
+@main
+struct Pizza_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
